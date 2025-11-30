5df4ab06e93df236cb268a13d551778d56660688
Nikoloz778 <abramishviliniko37@gmail.com>
2025-11-30 08:01:52 +0400

    097

---
+++ day(097)/classwork(97)/script.js
@@ -0,0 +1,17 @@
+console.log("Nikoloz Abramishvili");
+console.log("Mariam Abramishvili");
+console.log("Abesalom Abramishvili");
+console.log("Vano Ivanishvili");
+console.log("Ineza Bagaturia");
+console.log("Merab Shariqadze");
+console.log("Gela Afrasidze");
+console.log("Davit NIniashvili");
+console.log("Anzor Wurwumia");
+console.log("Donald Tramp");
+console.log("Elizbar Elizbarashvili");
+console.log("khvicha kvaracxelia");
+console.log("Giorgi Mamardashvili");
+console.log("Nino Sixarulizde");
+console.log("Otar Abramishvili")
+
+
---
+++ day(097)/homework(97)/scriptN1.js
@@ -0,0 +1,88 @@
+// 1)
+console.log("ძაღლი")
+console.log("კატა")
+console.log("ლომი")
+console.log("ვეფხვი")
+console.log("მგელი")
+console.log("ირემი")
+console.log("ჯიხვი")
+console.log("მელა")
+console.log("კურდღელი")
+console.log("ანტილოპა")
+
+// 2)
+// ცვლადის შექმნა შეიძლება ორი სახით var-ის, let-ის და const-ის გამოყენებით. აქედან პირველი მოძველებული ფორმაა და მეროე უფრო თანამედროვეა არსებობს პატარა განსხვავებებიც. რაც შეეხება მესამეს, ამ ეგრედწოდებული წინდებულით შექმნილი ცვლადის მნიშვნელობა მუდმივია და ვერ შეიცვლება ვერანაირად მხოლოდ საწყის ცვლადში თუ შევუცვლით მნიშვნელობას.
+
+// 3)
+var username = "nikoloz" 
+var surename = "abramishvili"
+var age = "18"
+var livingPlace = "Tbilisi"
+var favFood = "khinkali"
+let favSportToWatch = "Rugby"
+let favSportToPlay = "vollayball"
+let favSport1 = "Judo"
+let favSport2 = "Box"
+let favSport3 = "football"
+const religion = "Orthodox"
+const homeland = "Saqartvelo"
+const Love = " X.B "
+const fight = "to the end"
+const favPlace = "Mountains" 
+
+console.log(username)
+console.log(surename)
+console.log(age)
+console.log(livingPlace)
+console.log(favFood)
+console.log(favSportToWatch)
+console.log(favSportToPlay)
+console.log(favSport1)
+console.log(favSport2)
+console.log(favSport3)
+console.log(religion)
+console.log(homeland)
+console.log(Love)
+console.log(fight)
+console.log(favPlace)
+
+// 4)
+
+//ჯავსკრიპტში გვაქვს number(1 , 2 , 3 , 1.2, 1.5), booleans(true false), strings(nikoloz).
+
+// 5)
+
+let word = "I"
+let word1 = "you"
+let word2 = "us"
+let boolean = true
+let boolean1 = false
+let number = 30
+let number1 = 13
+let number2 = 3
+console.log(word)
+console.log(word1)
+console.log(word2)
+console.log(boolean)
+console.log(boolean1)
+console.log(number)
+console.log(number1)
+console.log(number2) 
+
+// 6)
+
+console.log(typeof word)
+console.log(typeof word1)
+console.log(typeof word2)
+console.log(typeof boolean)
+console.log(typeof boolean1)
+console.log(typeof number)
+console.log(typeof number1)
+console.log(typeof number2) 
+
+//7
+
+//გვაქვს +, -, *,  /, %.
+ 
+// 8)
+console.log(alert("be careful"))
